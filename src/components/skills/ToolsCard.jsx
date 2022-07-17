7d2f2131458beb21b4ed134a8f7ec5363c4b6e64
arwin.dev <arwinswapna@usf.edu>
2022-07-17 14:00:46 +0530

    prod

--- src/components/skills/ToolsCard.jsx
+++ src/components/skills/ToolsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({id,name,path}) => {
     return(
         <div className="mx-auto hover:translate-y-[-15px] duration-300">
-            <img src={path} alt="robots" style={{width: '100px'}}/>
+            <img src={path} alt="robots" style={{width: '150px'}}/>
             <h1 className='text-center pt-3'>{name}</h1>
         </div>
     );
