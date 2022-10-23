1d6f2c19af98c0af3f4bf5b718704e8c635a96ec
arwin-dev <arwin.dev@outlook.com>
2022-10-23 14:49:25 -0400

    leetcode update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Layout from "./components/layout/Layout"
 import Home from './components/home/Home'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
-import Leetcode from "./components/leetcode/Leetcode"
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 
@@ -38,7 +37,6 @@ class App extends Component {
               <Route index element={<Home/>}/>
               <Route path="/about" element={<About/>} />
               <Route path="/skills" element={<Skills data={items} />} />
-              <Route path="/leetcode" element={<Leetcode data={items} />}/>
               <Route path="/projects" element={<Projects/>} />
               <Route path="/contact" element={<Contact/>} />
             </Route>
